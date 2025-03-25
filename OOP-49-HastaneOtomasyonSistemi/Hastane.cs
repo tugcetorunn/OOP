20d41d0d10bbf1c7e7493d18e3a33b112048f729
Tuğçe Torun <74455637+tugcetorunn@users.noreply.github.com>
2025-03-25 21:35:28 +0300

    Create Hastane.cs

---
+++ OOP-49-HastaneOtomasyonSistemi/Hastane.cs
@@ -0,0 +1,5 @@
+public int HastaneId { get; set; }
+public string HastaneAdi { get; set; }
+public string Adres { get; set; }
+public List<Calisan> Calisanlar { get; set; }
+public List<Randevu> Randevular { get; set; }
