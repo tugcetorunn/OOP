bae08131b37fb2c448ceb5d68042cf0db7b56566
tugcetorunn <tokerrtugcee@gmail.com>
2025-03-25 21:04:41 +0300

    25.03-tt

---
+++ OOP-50-HastaneOtomasyonCalistirma/Program.cs
@@ -0,0 +1,10 @@
+﻿// bir hastane oop uygulaması isteniyor.
+// doktorlar, doktor sekreterleri, danışmanlar, vezneciler, hizmetliler, 1 başhekim.
+// doktorların dereceleri var - asistan, uzman, doç, prof
+// uzmanlık alanları - dahiliye, çocuk, kadın, üroloji, kardiyoloji, zührevi, onkoloji
+// hastaların özellikleri - ad, soyad, tc, yaş, randevular, 
+// randevu - hasta, doktor, gün-saat, durum
+// randevu sonuçları - randevu, durum, recete, tahliller, 
+// tahlil - randevuSonuc, 
+// recete - randevuSonuc, ilaçlar
+// ilaç - ad, 
