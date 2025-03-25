47287a1d1567eee62d655825b079be778b061602
Tuğçe Torun <74455637+tugcetorunn@users.noreply.github.com>
2025-03-25 21:25:41 +0300

    Update Program.cs

--- OOP-50-HastaneOtomasyonCalistirma/Program.cs
+++ OOP-50-HastaneOtomasyonCalistirma/Program.cs
@@ -2,9 +2,12 @@
 // doktorlar, doktor sekreterleri, danışmanlar, vezneciler, hizmetliler, 1 başhekim.
 // doktorların dereceleri var - asistan, uzman, doç, prof
 // uzmanlık alanları - dahiliye, çocuk, kadın, üroloji, kardiyoloji, zührevi, onkoloji
-// hastaların özellikleri - ad, soyad, tc, yaş, randevular, 
-// randevu - hasta, doktor, gün-saat, durum
+// hastaların özellikleri - ad, soyad, tc, yaş, randevular, sgk
+// randevu - tür, hasta, doktor, gün-saat, durum, randevuSonuc, ücret
+// ilk randevu - randevudan implement
+// kontrol randevusu - randevudan implement, (kontrol ile ilk arasında 15 günden az zaman varsa ücret 0)
 // randevu sonuçları - randevu, durum, recete, tahliller, 
-// tahlil - randevuSonuc, 
+// tahlil - randevuSonuc, değer türü, ölçüm, referans değeri
 // recete - randevuSonuc, ilaçlar
-// ilaç - ad, 
+// ilaç - ad, bileşenler
+// bileşen - ad, ilaçlar
